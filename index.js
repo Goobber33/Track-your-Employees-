32a5764dbb47c7e78f888b9b784ae10a87d34292
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-03-21 13:32:56 -0700

    Added variables, dependencies, and an inquirer prompt for the application.

---
+++ index.js
@@ -0,0 +1,24 @@
+const inquirer = require('inquirer');
+const database = require('./lib/database');
+
+function mainMenu() {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Quit'
+      ]
+    })
+
+}
+
+mainMenu();
